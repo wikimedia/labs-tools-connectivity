0998518f6c09cd6b31868237f937920182c13f18
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2007-11-09 23:55:19 +0000

    MySQL - bash API simplified
    git-svn-id: https://svn.toolserver.org/svnroot/golem@14 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/isolated.sh
+++ isolated/isolated.sh
@@ -9,20 +9,19 @@
 
 #!/bin/bash
 
-# debug level enable
-debugmode=
-debugshift=0
-if [ "$1" != "" ]
+do_apply=0
+if [ "$1" = "apply" ]
 then
-  # not yet work, to debug look at lines around 1159 in isolated.sql
-  debugmode="-v -v -v"
-  debugshift=2
+  do_apply=1
 fi
 
 dbhost="sql-s3"
-usr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
-pwd=$( cat ~/.my.cnf | grep 'password ' | sed 's/^password = \"\([^\"]*\)\"$/\1/' )
-sql="mysql --host=$dbhost -A --user=${usr} --password=${pwd} --database=u_${usr} -n"
+myusr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
+mypwd=$( cat ~/.my.cnf | grep 'password ' | sed 's/^password = \"\([^\"]*\)\"$/\1/' )
+sql="mysql --host=$dbhost -A --user=${myusr} --password=${mypwd} --database=u_${myusr} -n"
+
+ruusr=$( cat ~/.ru.cnf | grep 'user ' | sed 's/^user = \"\([^\"]*\)\"$/\1/' )
+rupwd=$( cat ~/.ru.cnf | grep 'password ' | sed 's/^password = \"\([^\"]*\)\"$/\1/' )
 
 rm -f ./*.info ./*.txt ./*.stat debug.log
 
@@ -31,19 +30,42 @@ handle ()
 {
   local line=$1
 
-  if [ "${line:$debugshift:3}" = ':: ' ]
+  if [ "${line:0:3}" = ':: ' ]
   then
-    if [ "${line:$debugshift+3:5}" = 'echo ' ]
+    # apply previous command if necessary
+    if [ "$state" = '2' ]
     then
-      echo ${line:$debugshift+8}
-    else
-      if [ "${line:$debugshift+3:4}" = 'out ' ]
+      if [ "$do_apply" = "1" ]
       then
-        out=${line:$debugshift+7}
-        state=1
+        echo $collectedline | perl r.pl $outpage "$ruusr" "$rupwd" 'pre'
+      fi
+    fi
+    state=0
+    # recognize current command
+    if [ "${line:3:5}" = 'echo ' ]
+    then
+      echo ${line:8}
+    else
+      if [ "${line:3:4}" = 'out ' ]
+      then
+        out=${line:7}
+        state=1 # file output
         if [ ! -f $out ]
         then
           echo -ne \\0357\\0273\\0277 > $out
+        fi
+      else
+        if [ "${line:3:7}" = 'upload ' ]
+        then
+          out=${line:10}
+          state=2 # upload and file output
+          collectedline=''
+          # need better way for url definition, maybe sql driven
+          outpage=${out:15:2}
+          if [ ! -f $out ]
+          then
+            echo -ne \\0357\\0273\\0277 > $out
+          fi
         fi
       fi
     fi
@@ -58,13 +80,11 @@ handle ()
            echo -ne $line\\r\\n >> debug.log
          fi
        fi;;
-    1) if [ "$line" = '' ]
-       then
-         state=0
-         echo -ne \\r\\n >> debug.log
-       else
-         echo -ne $line\\r\\n >> $out
-       fi;;
+    1) echo -ne $line\\r\\n >> $out
+       ;;
+    2) echo -ne $line\\r\\n >> $out
+       collectedline=$(echo -ne "${collectedline}\\r${line}")
+       ;;
     *) echo $line;;
     esac
   fi
@@ -82,6 +102,10 @@ time {
   rm -f ./*.info
 
   7z a stat.7z ./*.stat >>7z.log 2>&1
+  if [ "$do_apply" = "1" ]
+  then
+    tail --bytes=+4 ./*.stat | perl r.pl 'stat' "$ruusr" "$rupwd" 'stat'
+  fi
 
   todos 7z.log
 }
