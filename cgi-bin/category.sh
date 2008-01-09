25e080fd1b26c8d6feba492d25e4c991c99b6ce1
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-01-09 03:20:32 +0000

    selection results now parsed more correctly; new tools links
    git-svn-id: https://svn.toolserver.org/svnroot/golem@40 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/category.sh
+++ cgi-bin/category.sh
@@ -29,6 +29,23 @@ parse_query() #@ USAGE: parse_query var ...
   set +f
 }
 
+handle_category ()
+{
+  local line=$1
+  line=${line//_/ }
+  echo \<li\>\<a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\"\>$line\<\/a\> \<small\>\<a href=\"/~mashiah/cgi-bin/suggest.sh?title=$line\"\>[[suggest links]]\<\/a\>\<\/small\>\<\/li\>
+}
+
+handle_catlist ()
+{
+  local line=$1
+  local name=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\1/g' )
+  local volume=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\2/g' )
+  local percent=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\3/g' )
+  name=${name//_/ }
+  echo \<li\>\<a href=\"/~mashiah/cgi-bin/category.sh?category=$name\"\>$name\<\/a\>: $volume \($percent\%\)\<\/li\>
+}
+
 echo Content-type: text/html
 echo ""
 
@@ -38,7 +55,7 @@ cat << EOM
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
  <head>
-  <title>Isolated articles in $1 for ruwiki</title><link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
+  <title>Isolated articles for ruwiki</title><link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
   
   </style>
  </head>
@@ -58,64 +75,82 @@ cat << EOM
 The list of isolated articles is collected manually and updated on a daily
 basis or close to.<br>
 So, use once a day for each particular category name.<br><br>
-Example: Материалы БСЭ
-<!-- ISINDEX prompt="Category name:" action="/~mashiah/cgi-bin/category.sh" -->
+Example: Писатели России
 <FORM action=""/~mashiah/cgi-bin/category.sh"" method="post">
-<P>Category name: <INPUT name=category type="text"></P>
+<P><font color=red>Enter your category name: <INPUT name=category type="text"> and hit enter</font></P>
 </FORM>
 EOM
 
 usr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
 pwd=$( cat ~/.my.cnf | grep 'password ' | sed 's/^password = \"\([^\"]*\)\"$/\1/' )
-sql="mysql --host=sql-s3 -A --user=${usr} --password=${pwd} --database=u_${usr}"
+sql="mysql --host=sql-s3 -A --user=${usr} --password=${pwd} --database=u_${usr} -N"
 
 parse_query category
+
+categoryurl=${category//\"/\%22}
+categorysql=${category//\"/\"\'\"\'\"}
 
 if [ "$category" != '' ]
 then
   echo "<h4>$category</h4>"
-  convertedcat=$( echo $category | sed -e 's/ /_/g' )
+  echo "this is a list of all isolated articles for the category,<br />"
+  echo "see also:"
+  echo "<ul>"
+  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=disambig\">list of isolated articles <b>linked from linked disambiguation pages</b></a></li>"
+  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=interlink\">list of isolated articles <b>with linking suggestions</b></a></li>"
+  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=translate\">list of isolated articles <b>with translation suggestions</b></a></li>"
+  echo "</ul>"
+  convertedcat=$( echo $categorysql | sed -e 's/ /_/g' )
 
   echo "<ol>"
-  echo SELECT title                                \
-              FROM ruwiki_p.categorylinks,         \
-                   ruwiki0                         \
-                   WHERE id=cl_from and            \
-                         cl_to=\'${convertedcat}\' \
-                   ORDER BY title ASC\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\<a href="http\:\/\/ru.wikipedia.org\/wiki\/\1" target=\"_blank\"\>\1\<\/a\>\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT title                                \
+                FROM ruwiki_p.categorylinks,         \
+                     ruwiki0                         \
+                     WHERE id=cl_from and            \
+                           cl_to=\"${convertedcat}\" \
+                     ORDER BY title ASC\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_category "$line"
+                    done
+                  }
   echo "</ol>"
   echo "end of the list"
 else
   echo "<h4>top 100 categories on isolated pages count</h4>"
 
   echo "<ul>"
-  echo SELECT CONCAT\( \'\<a href=\"\/\~mashiah\/cgi-bin\/category.sh\?category=\', cl_to, \'\" target=\"blank\"\>\', cl_to, \'\<\/a\>\' \), \
-              count\( \*\ \) as cnt,                                                                                                         \
-              CONCAT\( \'\(\',100\*count\(\*\)/cv_count,\'\%\)\' \)                                                                          \
-              FROM ruwiki_p.categorylinks,                                                                                                   \
-                   ruwiki0,                                                                                                                  \
-                   catvolume                                                                                                                 \
-                   WHERE id=cl_from and                                                                                                      \
-                         cv_title=cl_to                                                                                                      \
-                   GROUP BY cl_to                                                                                                            \
-                   ORDER BY cnt DESC                                                                                                         \
-                   LIMIT 100\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\1\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT cv_title,                 \
+                cv_isocount,              \
+                100\*cv_isocount/cv_count \
+                FROM catvolume            \
+                ORDER BY cv_isocount DESC \
+                LIMIT 100\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_catlist "$line"
+                    done
+                  }
   echo "</ul>"
   echo "end of the list"
+
   echo "<h4>top 100 categories on isolated pages ratio</h4>"
 
   echo "<ul>"
-  echo SELECT CONCAT\( \'\<a href=\"\/\~mashiah\/cgi-bin\/category.sh\?category=\', cl_to, \'\" target=\"blank\"\>\', cl_to, \'\<\/a\>\' \), \
-              count\(\*\) as cnt,                                                                                                            \
-              CONCAT\(\'\(\',100\*count\(\*\)/cv_count,\'\%\)\'\)                                                                            \
-              FROM ruwiki_p.categorylinks,                                                                                                   \
-                   ruwiki0,                                                                                                                  \
-                   catvolume                                                                                                                 \
-                   WHERE id=cl_from and                                                                                                      \
-                         cv_title=cl_to                                                                                                      \
-                   GROUP BY cl_to                                                                                                            \
-                   ORDER BY 100\*count\(\*\)/cv_count DESC, cnt DESC                                                                         \
-                   LIMIT 100\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\1\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT cv_title,                            \
+                cv_isocount,                         \
+                100\*cv_isocount/cv_count as pcnt    \
+                FROM catvolume                       \
+                ORDER BY pcnt DESC, cv_isocount DESC \
+                LIMIT 100\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_catlist "$line"
+                    done
+                  }
   echo "</ul>"
   echo "end of the list"
 fi
@@ -124,6 +159,7 @@ cat << EOM
 
 <h2><a href="./category14.sh">categorytree connectivity (ruwiki)</a></h2>
 <h2><a href="./suggest.sh">suggest a link for isolated articles (ruwiki)</a></h2>
+<h2><a href="./creators.sh">isolated articles creators (ruwiki)</a></h2>
  <body>
 </html>
 EOM
