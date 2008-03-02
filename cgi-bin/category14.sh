3bd2c7e9a2a28105dae5accf24b7fd0fa2dc9b86
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-03-02 02:30:24 +0000

    redesign plus menu
    git-svn-id: https://svn.toolserver.org/svnroot/golem@66 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/category14.sh
+++ cgi-bin/category14.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-# Mashiah Davidson: # Thanks a lot to Chris F.A. Johnson for my time saved.
-# This function copied as is from 
-# http://www.unixreview.com/documents/s=10116/ur0701i/parse-query.sh.txt
-parse_query() #@ USAGE: parse_query var ...                                                                 
+source ./common
+
+parse_query networkpath
+parse_query interface
+if [ "$interface" != 'ru' ]
+then
+  interface='en'
+fi
+
+source ./common.$interface
+source ./category14.$interface
+source ./common2
+
+handle_layer ()
 {
-    local var val
-    local IFS='&'
-    vars="&$*&"
-    [ "$REQUEST_METHOD" = "POST" ] && read QUERY_STRING
-    set -f
-    for item in $QUERY_STRING
-    do
-      var=${item%%=*}
-      val=${item#*=}
-      val=${val//+/ }
-      case $vars in
-          *"&$var&"* )
-              case $val in
-                  *%[0-9a-fA-F][0-9a-fA-F]*)
-                       val=$( printf "%b" "${val//\%/\\x}." )
-                       val=${val%.}
-              esac
-              eval "$var=\$val"
-              ;;
-      esac
-    done
-    set +f
+  local line=$1
+
+  if no_sql_error "$line"
+  then
+    line=${line//_/ }
+    echo \<li\>\<a href=\"http://ru.wikipedia.org/w/index.php?title=Категория:$line\" target=\"_blank\"\>$line\<\/a\>\<\/li\>
+  fi
+}
+
+handle_table ()
+{
+  local line=$1
+
+  if no_sql_error "$line"
+  then
+    local lname=$( echo $line | sed -e 's/^\(.*\)\s\([1-9][0-9]*\)/\1/g' )
+    local amnt=$( echo $line | sed -e 's/^\(.*\)\s\([1-9][0-9]*\)/\2/g' )
+    echo "<a href='/~mashiah/cgi-bin/category14.sh?interface=$interface&networkpath=$lname'>$lname</a>:&nbsp;$amnt<br />"
+  fi
+}
+
+handle_rlist ()
+{
+  local line=$1
+
+  if no_sql_error "$line"
+  then
+    line=${line//_/ }
+    echo \<li\>\<a href=\"http://ru.wikipedia.org/w/index.php?title=Категория:$line\&redirect=no\" target=\"_blank\"\>$line\<\/a\>\<\/li\>
+  fi
 }
 
 echo Content-type: text/html
@@ -38,31 +56,90 @@ cat << EOM
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
  <head>
-  <title>Categorytree connectivity for ruwiki</title><link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+EOM
+
+echo "<title>$pagetitle</title>"
+
+cat << EOM
+  <link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
   
   </style>
  </head>
  <body>
-  <a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by
-    Wikimedia-Toolserver" /></a>
-  <h1><a href="/~mashiah">Mashiah's Projects</a></h1>
-
-<h2>contact</h2>
-<ul>
-<li>see <a href="http://ru.wikipedia.org/wiki/User:Mashiah_Davidson">my russian userpage</a>, <a href="http://ru.wikipedia.org/wiki/User:Голем">my bot's page</a> and <a href="http://ru.wikipedia.org/wiki/User Talk:Голем">our shared discussion</a></li>
-<li>look for me (nick: mashiah) in <a href="irc://irc.freenode.net/wikimedia-toolserver">#wikimedia-toolserver</a> on freenode</li>
-</ul>
-
-
-<h2>categorytree connectivity (ruwiki)</h2>
-The categorytree structure collection is run manually and updated daily or close to.<br>
-So, use once a day before a notification.<br><br>
+<a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by Wikimedia-Toolserver" /></a>
 EOM
 
-usr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
-sql="mysql --defaults-file=/home/mashiah/.my.cnf --host=sql-s3 -A --database=u_${usr}"
+echo "<h1>$mainh1</h1>"
+echo "<table><tr><td width=25% border=10>"
+echo -ne "<h1>"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "<a href=\"/~mashiah/cgi-bin/category14.sh?interface=en&networkpath=$networkpath\">[[en:]]</a> [[ru:]]"
+else
+  echo -ne "[[en:]] <a href=\"/~mashiah/cgi-bin/category14.sh?interface=ru&networkpath=$networkpath\">[[ru:]]</a>"
+fi
+echo "</h1>"
+echo -ne "<b><a href=\"/~mashiah/index"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">1) $motivation</a></b><br />"
+echo "<br />"
+echo "<b>2) <a href=\"http://ru.wikipedia.org/w/index.php?title=$isourl\">$isolatedarticles</a></b><br />"
+echo "<ul>"
+echo "<li><b><a href=\"/~mashiah/cgi-bin/category.sh?interface=$interface\">$bycategory</a></b></li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface\">$allsuggestions</a></li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=disambigcat\">$resolvedisambigs</a></li>"
+echo "<li>$justlink</li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=interlinkcat\">$parttranslate</a></li>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=translatecat\">$translatenlink</a></li>"
+echo "</ul>"
+echo "</ul>"
+echo "</ul>"
+echo -ne "<li><b><a href=\"/~mashiah/lists"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">$wholelist</a></b></li>"
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/bytypes\">$byclastertype</a></b></li>"
+echo "<ul><li><a href=\"http://ru.wikipedia.org/w/index.php?title=$orphurl\">$orphanes</a></li></ul>"
+echo "<li><b><a href=\"/~mashiah/cgi-bin/creators.sh?interface=$interface\">$bycreator</a></b></li>"
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/cltgdata\">$graphdata</a></b></li>"
+echo "</ul>"
+echo "<br />"
+echo "<b>3) <a href=\"http://ru.wikipedia.org/w/index.php?title=$deadendurl\">$deadend</a></b><br />"
+echo "<br />"
+echo "<b>4) <a href=\"/~mashiah/cgi-bin/disambig.sh?interface=$interface\">$disambig</a></b><br />"
+echo "<br />"
+if [ "$networkpath" = '' ]
+then
+  echo "<b><font color=red>5) $cattreecon</font></b><br />"
+else
+  echo "<b>5) <a href=\"/~mashiah/cgi-bin/category14.sh?interface=$interface\">$cattreecon</a></b><br />"
+  echo "<ul>"
+  echo "<li><font color=red><small>$networkpath</small></font></li>"
+  echo "</ul>"
+fi
+echo "<br />"
+echo "<b>6) $contactme</b><br />"
+echo "<ul>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:Mashiah_Davidson\">$mywikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$botwikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User Talk:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$commondisc</a></li>"
+echo "<li>mashiah $attext <a href="irc://irc.freenode.net/$ircchan">#$ircchan</a></li>"
+echo "</ul>"
+echo "<p align=justify>$srclocation <a href="http://fisheye.ts.wikimedia.org/browse/mashiah">toolserver fisheye</a>.</p>"
+echo "</td><td width=75%>"
+echo "<h1>$thish1</h1>"
 
-parse_query networkpath
+echo $actualnote1
+echo $actualnote2
 
 if [ "$networkpath" != '' ]
 then
@@ -71,80 +148,103 @@ then
   if [ "$networkpath" = '_1' ]
   then
     echo "<font color=red>"
-    echo "The list below normally must contain just one category named <font color=green>Всё</font>.<br />"
-    echo "Other entries are considered as uncotegorized categories.<br />"
+    echo $rootcatnote1
+    echo $rootcatnote2
     echo "</font>"
   fi
 
   if [[ "$networkpath" =~ '^.*\_([1-9][1-90]+|[2-9])$' ]]
   then
     echo "<font color=red>"
-    echo "The list below shows categories forming clusters of size ${BASH_REMATCH[1]}.<br />"
-    echo "Normally it must be empty (and unexistent), there must be no clusters of size above one in the categorytree.<br />"
+    echo "$clsizenote1 ${BASH_REMATCH[1]}.<br />"
+    echo $clsizenote2
     echo "</font>"
   fi
 
   echo "<ol>"
-  echo SELECT title                           \
-              FROM ruwiki14                   \
-                   WHERE cat=\'$networkpath\' \
-                   ORDER BY title ASC\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\<a href="http\:\/\/ru.wikipedia.org\/wiki\/Категория:\1" target=\"_blank\"\>\1\<\/a\>\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT title                           \
+                FROM ruwiki14                   \
+                     WHERE cat=\'$networkpath\' \
+                     ORDER BY title ASC\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_layer "$line"
+                    done
+                  }
   echo "</ol>"
-  echo "end of the list"
+  echo $listend
 else
-  echo "Each row in the table represents a layer in the categorytree."
-  echo "Normally layers are formed by categorization of categories and must have no cycles."
-  echo "All the layers having a number higher than 1 at the end of the layer name are wrong and represent cycles."
-  echo "Also it is important to keep the top layer category _1 having the single top member.<br />"
+  echo $cattreedesc1
+  echo $cattreedesc2
+  echo $cattreedesc3
+  echo $cattreedesc4
 fi
 
-echo "<center><table border=0><tr><th>Categorytree structure</th></tr><tr><td><small>"
-echo SELECT coolcat,            \
-            count\(cat\) as cnt \
-            FROM orcat14,       \
-                 ruwiki14       \
-            WHERE coolcat=cat   \
-            GROUP BY cat        \
-            ORDER BY coolcat ASC\; | $sql -N | sed -e 's/^\(.*\)\s\([1-9][0-9]*\)$/\<a href="\/\~mashiah\/cgi-bin\/category14.sh\?networkpath=\1"\>\1\<\/a\>:\&nbsp\;\2\<br \/\>/'
+echo "<center><table border=0><tr><th>$struchead</th></tr><tr><td align=center><small>"
+{
+  echo SELECT coolcat,            \
+              count\(cat\) as cnt \
+              FROM orcat14,       \
+                   ruwiki14       \
+              WHERE coolcat=cat   \
+              GROUP BY cat        \
+              ORDER BY REPLACE\(coolcat,\'_\',\'\+\'\) ASC\;
+} | $sql 2>&1 | { 
+                  while read -r line
+                    do handle_table "$line"
+                  done
+                }
 echo "</small></td></tr></table></center>"
 
 if [ "$networkpath" = '' ]
 then
-  echo "<h4>redirects</h4>"
+  echo "<h4>$queryname1</h4>"
   echo "<font color=red>"
-  echo "Categories namespace is not designed for redirects.<br />"
-  echo "Categorization through a redirect does not construct proper category lists.<br />"
+  echo $query1note1
+  echo $query1note2
   echo "</font>"
   
   echo "<ol>"
-  echo SELECT r_title  \
-              FROM r14 \
-              ORDER BY r_title ASC\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\<a href="http\:\/\/ru.wikipedia.org\/w\/index.php\?title=Категория:\1\&redirect=no" target=\"_blank\"\>\1\<\/a\>\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT r_title  \
+                FROM r14 \
+                ORDER BY r_title ASC\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_rlist "$line"
+                    done
+                  }
   echo "</ol>"
-  echo "end of the list"
+  echo $listend
 
-  echo "<h4>redirects with more than one outgoing link</h4>"
+  echo "<h4>$queryname2</h4>"
   echo "<font color=red>"
-  echo "This list contains maybe a bit more complex for resolving type of redirects.<br />"
-  echo "It contains redirects in category namespace with articles included into this redirec as in a category, or even redirects with superflous text and links, which are normally invisible.<br />"
-  echo "Redirects to main namespace with articles included are probably not redirects, but regular categories.<br />"
-  echo "Other redirects linking categories need to be emptied before redirect deletion.<br />"
+  echo $query2note1
+  echo "<ul>"
+  echo "<li>$query2note2</li>"
+  echo "<li>$query2note3</li>"
+  echo "</ul>"
   echo "</font>"
   
   echo "<ol>"
-  echo SELECT wr_title  \
-              FROM wr14 \
-              ORDER BY wr_title ASC\; | $sql -N | sed -e 's/^\(.*\)$/\<li\>\<a href="http\:\/\/ru.wikipedia.org\/w\/index.php\?title=Категория:\1\&redirect=no" target=\"_blank\"\>\1\<\/a\>\<\/li\>/' -e 's/_/ /g'
+  {
+    echo SELECT wr_title  \
+                FROM wr14 \
+                ORDER BY wr_title ASC\;
+  } | $sql 2>&1 | { 
+                    while read -r line
+                      do handle_rlist "$line"
+                    done
+                  }
   echo "</ol>"
-  echo "end of the list"
+  echo $listend
 fi
 
-
 cat << EOM
-
-<h2><a href="./category.sh">isolated articles for a particular category (ruwiki)</a></h2>
-<h2><a href="./suggest.sh">suggest a link for isolated articles (ruwiki)</a></h2>
-<h2><a href="./creators.sh">isolated articles creators (ruwiki)</a></h2>
+</td>
+</tr>
+</table>
 
  <body>
 </html>
