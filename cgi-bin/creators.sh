36d09df9c1ae2649dd5df2109b57ba8cb3eb6cb5
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-03-02 02:29:55 +0000

    redesign plus menu
    git-svn-id: https://svn.toolserver.org/svnroot/golem@65 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/creators.sh
+++ cgi-bin/creators.sh
@@ -1,48 +1,42 @@
 #!/bin/bash
 
-# Mashiah Davidson: # Thanks a lot to Chris F.A. Johnson for my time saved.
-# This function copied as is from 
-# http://www.unixreview.com/documents/s=10116/ur0701i/parse-query.sh.txt
-parse_query() #@ USAGE: parse_query var ...                                                                 
-{
-  local var val
-  local IFS='&'
-  vars="&$*&"
-  [ "$REQUEST_METHOD" = "POST" ] && read QUERY_STRING
-  set -f
-  for item in $QUERY_STRING
-  do
-    var=${item%%=*}
-    val=${item#*=}
-    val=${val//+/ }
-    case $vars in
-      *"&$var&"* )
-        case $val in
-          *%[0-9a-fA-F][0-9a-fA-F]*)
-            val=$( printf "%b" "${val//\%/\\x}." )
-            val=${val%.}
-        esac
-        eval "$var=\$val"
-        ;;
-    esac
-  done
-  set +f
-}
+source ./common
+
+parse_query user
+parse_query interface
+parse_query registered
+parse_query shift
+if [ "$interface" != 'ru' ]
+then
+  interface='en'
+fi
+
+source ./common.$interface
+source ./creators.$interface
+source ./common2
 
 handle_isolates ()
 {
   local line=$1
-  line=${line//_/ }
-  echo \<li\>\<a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\"\>$line\<\/a\> \<small\>\<a href=\"/~mashiah/cgi-bin/suggest.sh?title=$line\"\>[[suggest links]]\<\/a\>\<\/small\>\<\/li\>
+
+  if no_sql_error "$line"
+  then
+    line=${line//_/ }
+    echo "<li><a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\">$line</a> <small><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&title=$line\"><font color=green>[[$suggest]]</font></a></small></li>"
+  fi
 }
 
 handle_userlist ()
 {
   local line=$1
-  local utxt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\1/g' )
-  local uid=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\2/g' )
-  local amnt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\3/g' )
-  echo "<li><a href='/~mashiah/cgi-bin/creators.sh?user=$utxt&registered=$uid'>$utxt</a>: $amnt</li>"
+
+  if no_sql_error "$line"
+  then
+    local utxt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\1/g' )
+    local uid=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\2/g' )
+    local amnt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\3/g' )
+    echo "<li><a href='/~mashiah/cgi-bin/creators.sh?interface=$interface&user=$utxt&registered=$uid'>$utxt</a>: $amnt</li>"
+  fi
 }
 
 echo Content-type: text/html
@@ -54,36 +48,97 @@ cat << EOM
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
  <head>
-  <title>Isolated articles creators for ruwiki</title><link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+EOM
+
+echo "<title>$pagetitle</title>"
+
+cat << EOM
+   
+  <link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
   
   </style>
  </head>
  <body>
-  <a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by
-    Wikimedia-Toolserver" /></a>
-  <h1><a href="/~mashiah">Mashiah's Projects</a></h1>
-
-<h2>contact</h2>
-<ul>
-<li>see <a href="http://ru.wikipedia.org/wiki/User:Mashiah_Davidson">my russian userpage</a>, <a href="http://ru.wikipedia.org/wiki/User:Голем">my bot's page</a> and <a href="http://ru.wikipedia.org/wiki/User Talk:Голем">our shared discussion</a></li>
-<li>look for me (nick: mashiah) in <a href="irc://irc.freenode.net/wikimedia-toolserver">#wikimedia-toolserver</a> on freenode</li>
-</ul>
-
-<h2>isolated articles creators (ruwiki)</h2>
-This page contains the lists of users who created isolated articles, it is
-ordered by the amount of isolated articles created.<br>
-Example: Maximaximax
-<FORM action=""/~mashiah/cgi-bin/creators.sh"" method="post">
-<P><font color=red>Enter a user name: <INPUT name=user type="text"> and hit enter</font></P>
-</FORM>
+<a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by Wikimedia-Toolserver" /></a>
 EOM
 
-usr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
-sql="mysql --defaults-file=/home/mashiah/.my.cnf --host=sql-s3 -A --database=u_${usr} -N"
+echo "<h1>$mainh1</h1>"
+echo "<table><tr><td width=25% border=10>"
+echo -ne "<h1>"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "<a href=\"/~mashiah/cgi-bin/creators.sh?interface=en&user=$user&registered=$registered&shift=$shift\">[[en:]]</a> [[ru:]]"
+else
+  echo -ne "[[en:]] <a href=\"/~mashiah/cgi-bin/creators.sh?interface=ru&user=$user&registered=$registered&shift=$shift\">[[ru:]]</a>"
+fi
+echo "</h1>"
+echo -ne "<b><a href=\"/~mashiah/index"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">1) $motivation</a></b><br />"
+echo "<br />"
+echo "<b>2) <a href=\"http://ru.wikipedia.org/w/index.php?title=$isourl\">$isolatedarticles</a></b><br />"
+echo "<ul>"
+echo "<li><b><a href=\"/~mashiah/cgi-bin/category.sh?interface=$interface\">$bycategory</a></b></li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface\">$allsuggestions</a></li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=disambigcat\">$resolvedisambigs</a></li>"
+echo "<li>$justlink</li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=interlinkcat\">$parttranslate</a></li>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=translatecat\">$translatenlink</a></li>"
+echo "</ul>"
+echo "</ul>"
+echo "</ul>"
+echo -ne "<li><b><a href=\"/~mashiah/lists"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">$wholelist</a></b></li>"
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/bytypes\">$byclastertype</a></b></li>"
+echo "<ul><li><a href=\"http://ru.wikipedia.org/w/index.php?title=$orphurl\">$orphanes</a></li></ul>"
+if [ "$user" = '' ]
+then
+  echo "<li><b><font color=red>$bycreator</font></b></li>"
+else
+  echo "<li><b><a href=\"/~mashiah/cgi-bin/creators.sh?interface=$interface\">$bycreator</a></b></li>"
+  echo "<ul>"
+  echo "<li><font color=red>${usrns}:$user</font></li>"
+  echo "</ul>"
+fi
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/cltgdata\">$graphdata</a></b></li>"
+echo "</ul>"
+echo "<br />"
+echo "<b>3) <a href=\"http://ru.wikipedia.org/w/index.php?title=$deadendurl\">$deadend</a></b><br />"
+echo "<br />"
+echo "<b>4) <a href=\"/~mashiah/cgi-bin/disambig.sh?interface=$interface\">$disambig</a></b><br />"
+echo "<br />"
+echo "<b>5) <a href=\"/~mashiah/cgi-bin/category14.sh?interface=$interface\">$cattreecon</a></b><br />"
+echo "<br />"
+echo "<b>6) $contactme</b><br />"
+echo "<ul>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:Mashiah_Davidson\">$mywikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$botwikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User Talk:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$commondisc</a></li>"
+echo "<li>mashiah $attext <a href="irc://irc.freenode.net/$ircchan">#$ircchan</a></li>"
+echo "</ul>"
+echo "<p align=justify>$srclocation <a href="http://fisheye.ts.wikimedia.org/browse/mashiah">toolserver fisheye</a>.</p>"
 
-parse_query user
-parse_query registered
-parse_query shift
+echo "</td><td width=75%>"
+
+echo "<h1>$thish1</h1>"
+
+echo "$whatisit<br><br>"
+echo $example
+echo "<FORM action=\"/~mashiah/cgi-bin/creators.sh\" method=\"get\">"
+echo "<INPUT type=hidden name=\"interface\" value=\"$interface\">"
+echo "<P><font color=red>$unamereq: <INPUT name=user type=\"text\"> $unamedo</font></P>"
+echo "</FORM>"
 
 shiftnext=$((shift+100))
 shiftprev=$((shift-100))
@@ -95,33 +150,34 @@ usersql=${user//\"/\"\'\"\'\"}
 
 if [ "$user" != '' ]
 then
+  echo -ne "<br />$list1expl "
   if [ "$registered" = '0' ]
   then
-    echo "<h4><a href='http://ru.wikipedia.org/wiki/Special:Contributions/$user' target=\"_blank\">$user</a></h4>"
+    echo "<a href='http://ru.wikipedia.org/wiki/Special:Contributions/$user' target=\"_blank\">$user</a>"
   else
-    echo "<h4><a href=\"http://ru.wikipedia.org/w/index.php?title=User:$userurl\" target=\"_blank\">$user</a></h4>"
+    echo "<a href=\"http://ru.wikipedia.org/w/index.php?title=User:$userurl\" target=\"_blank\">$user</a>"
   fi
-  echo "this is the list of isolated articles created by $user"
   echo "<ol>"
   {
     echo SELECT title                       \
                 FROM creators               \
                 WHERE user_text=\"${usersql}\" \
-                ORDER BY title ASC;
+                ORDER BY title ASC\;
   } | $sql 2>&1 | { 
                     while read -r line
                       do handle_isolates "$line"
                     done
                   }
   echo "</ol>"
+  echo $listend
 else
-  echo "<h4>top 100 users on amount of isolated articles created</h4>"
+  echo "<h4>$list2name</h4>"
 
   if [ $((shift)) -gt 0 ]
   then
-    echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftprev\">previous 100</a> "
+    echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftprev&interface=$interface\">$previous 100</a> "
   fi
-  echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftnext\">next 100</a>"
+  echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftnext&interface=$interface\">$next 100</a>"
   echo "<ol start=$((shift+1))>"
   {
     echo SELECT user_text,         \
@@ -139,16 +195,16 @@ else
   echo "</ol>"
   if [ $((shift)) -gt 0 ]
   then
-    echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftprev\">previous 100</a> "
+    echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftprev&interface=$interface\">$previous 100</a> "
   fi
-  echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftnext\">next 100</a>"
+  echo "<a href=\"/~mashiah/cgi-bin/creators.sh?shift=$shiftnext&interface=$interface\">$next 100</a>"
 fi
 
 cat << EOM
+</td>
+</tr>
+</table>
 
-<h2><a href="./category.sh">isolated articles for a particular category (ruwiki)</a></h2>
-<h2><a href="./category14.sh">categorytree connectivity (ruwiki)</a></h2>
-<h2><a href="./suggest.sh">suggest a link for isolated articles (ruwiki)</a></h2>
  <body>
 </html>
 EOM
