6911ed91b166b5b7e4c224774ccd9cc31a3999da
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-03-02 02:30:45 +0000

    redesign plus menu
    git-svn-id: https://svn.toolserver.org/svnroot/golem@67 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/category.sh
+++ cgi-bin/category.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
 
-# Mashiah Davidson: # Thanks a lot to Chris F.A. Johnson for my time saved.
-# This function copied as is from 
-# http://www.unixreview.com/documents/s=10116/ur0701i/parse-query.sh.txt
-parse_query() #@ USAGE: parse_query var ...                                                                 
-{
-  local var val
-  local IFS='&'
-  vars="&$*&"
-  [ "$REQUEST_METHOD" = "POST" ] && read QUERY_STRING
-  set -f
-  for item in $QUERY_STRING
-  do
-    var=${item%%=*}
-    val=${item#*=}
-    val=${val//+/ }
-    case $vars in
-      *"&$var&"* )
-        case $val in
-          *%[0-9a-fA-F][0-9a-fA-F]*)
-            val=$( printf "%b" "${val//\%/\\x}." )
-            val=${val%.}
-        esac
-        eval "$var=\$val"
-        ;;
-    esac
-  done
-  set +f
-}
+source ./common
+
+parse_query category
+parse_query interface
+if [ "$interface" != 'ru' ]
+then
+  interface='en'
+fi
+
+source ./common.$interface
+source ./category.$interface
+source ./common2
 
 handle_category ()
 {
   local line=$1
-  line=${line//_/ }
-  echo \<li\>\<a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\"\>$line\<\/a\> \<small\>\<a href=\"/~mashiah/cgi-bin/suggest.sh?title=$line\"\>[[suggest links]]\<\/a\>\<\/small\>\<\/li\>
+
+  if no_sql_error "$line"
+  then
+    line=${line//_/ }
+    echo "<li><a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\">$line</a> <small><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&title=$line\"><font color=green>[[$suggest]]</font></a></small></li>"
+  fi
 }
 
 handle_catlist ()
 {
   local line=$1
-  local name=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\1/g' )
-  local volume=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\2/g' )
-  local percent=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\3/g' )
-  name=${name//_/ }
-  echo \<li\>\<a href=\"/~mashiah/cgi-bin/category.sh?category=$name\"\>$name\<\/a\>: $volume \($percent\%\)\<\/li\>
+
+  if no_sql_error "$line"
+  then
+    local name=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\1/g' )
+    local volume=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\2/g' )
+    local percent=$( echo $line | sed -e 's/^\([^ ]\+\) \([^ ]\+\) \([^ ]\+\)/\3/g' )
+    name=${name//_/ }
+    echo "<li><a href=\"/~mashiah/cgi-bin/category.sh?interface=$interface&category=$name\">$name</a>: $volume ($percent%)</li>"
+  fi
 }
 
 echo Content-type: text/html
@@ -55,50 +47,115 @@ cat << EOM
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
  <head>
-  <title>Isolated articles for ruwiki</title><link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+EOM
+
+echo "<title>$pagetitle</title>"
+
+cat << EOM
+  
+  <link rel="stylesheet" type="text/css" href="/~mashiah/main.css" media="all" /><style type="text/css">
   
   </style>
  </head>
  <body>
-  <a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by
-    Wikimedia-Toolserver" /></a>
-  <h1><a href="/~mashiah">Mashiah's Projects</a></h1>
-
-<h2>contact</h2>
-<ul>
-<li>see <a href="http://ru.wikipedia.org/wiki/User:Mashiah_Davidson">my russian userpage</a>, <a href="http://ru.wikipedia.org/wiki/User:Голем">my bot's page</a> and <a href="http://ru.wikipedia.org/wiki/User Talk:Голем">our shared discussion</a></li>
-<li>look for me (nick: mashiah) in <a href="irc://irc.freenode.net/wikimedia-toolserver">#wikimedia-toolserver</a> on freenode</li>
-</ul>
-
-
-<h2>isolated articles for a particular category (ruwiki)</h2>
-The list of isolated articles is collected manually and updated on a daily
-basis or close to.<br>
-So, use once a day for each particular category name.<br><br>
-Example: Писатели России
-<FORM action=""/~mashiah/cgi-bin/category.sh"" method="post">
-<P><font color=red>Enter your category name: <INPUT name=category type="text"> and hit enter</font></P>
-</FORM>
+<a href="/"><img id="poweredbyicon" src="/~mashiah/wikimedia-toolserver-button.png" alt="Powered by Wikimedia-Toolserver" /></a>
 EOM
 
-usr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
-sql="mysql --defaults-file=/home/mashiah/.my.cnf --host=sql-s3 -A --database=u_${usr} -N"
+echo "<h1>$mainh1</h1>"
+echo "<table><tr><td width=25% border=10>"
+echo -ne "<h1>"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "<a href=\"/~mashiah/cgi-bin/category.sh?interface=en&category=$category\">[[en:]]</a> [[ru:]]"
+else
+  echo -ne "[[en:]] <a href=\"/~mashiah/cgi-bin/category.sh?interface=ru&category=$category\">[[ru:]]</a>"
+fi
+echo "</h1>"
+echo -ne "<b><a href=\"/~mashiah/index"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">1) $motivation</a></b><br />"
+echo "<br />"
+echo "<b>2) <a href=\"http://ru.wikipedia.org/w/index.php?title=$isourl\">$isolatedarticles</a></b><br />"
+echo "<ul>"
+if [ "$category" = '' ]
+then
+  echo "<li><b><font color=red>$bycategory</font></b></li>"
+else
+  echo "<li><b><a href=\"/~mashiah/cgi-bin/category.sh?interface=$interface\">$bycategory</a></b></li>"
+fi
+echo "<ul>"
+if [ "$category" != '' ]
+then
+  echo "<li><font color=red>${catns}:$category</font></li>"
+fi
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface\">$allsuggestions</a></li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=disambigcat\">$resolvedisambigs</a></li>"
+if [ "$category" != '' ]
+then
+  echo "<ul><li><a id=seealso href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&category=$category&suggest=disambig\">${catns}:$category</a></li></ul>"
+fi
+echo "<li>$justlink</li>"
+echo "<ul>"
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=interlinkcat\">$parttranslate</a></li>"
+if [ "$category" != '' ]
+then
+  echo "<ul><li><a id=seealso href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&category=$category&suggest=interlink\">${catns}:$category</a></li></ul>"
+fi
+echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&listby=translatecat\">$translatenlink</a></li>"
+if [ "$category" != '' ]
+then
+  echo "<ul><li><a id=seealso href=\"/~mashiah/cgi-bin/suggest.sh?interface=$interface&category=$category&suggest=translate\">${catns}:$category</a></li></ul>"
+fi
+echo "</ul>"
+echo "</ul>"
+echo "</ul>"
+echo -ne "<li><b><a href=\"/~mashiah/lists"
+if [ "$interface" = 'ru' ]
+then
+  echo -ne "ru"
+fi
+echo ".html\">$wholelist</a></b></li>"
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/bytypes\">$byclastertype</a></b></li>"
+echo "<ul><li><a href=\"http://ru.wikipedia.org/w/index.php?title=$orphurl\">$orphanes</a></li></ul>"
+echo "<li><b><a href=\"/~mashiah/cgi-bin/creators.sh?interface=$interface\">$bycreator</a></b></li>"
+echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/cltgdata\">$graphdata</a></b></li>"
+echo "</ul>"
+echo "<br />"
+echo "<b>3) <a href=\"http://ru.wikipedia.org/w/index.php?title=$deadendurl\">$deadend</a></b><br />"
+echo "<br />"
+echo "<b>4) <a href=\"/~mashiah/cgi-bin/disambig.sh?interface=$interface\">$disambig</a></b><br />"
+echo "<br />"
+echo "<b>5) <a href=\"/~mashiah/cgi-bin/category14.sh?interface=$interface\">$cattreecon</a></b><br />"
+echo "<br />"
+echo "<b>6) $contactme</b><br />"
+echo "<ul>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:Mashiah_Davidson\">$mywikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$botwikipage</a></li>"
+echo "<li><a href=\"http://ru.wikipedia.org/wiki/User Talk:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$commondisc</a></li>"
+echo "<li>mashiah $attext <a href="irc://irc.freenode.net/$ircchan">#$ircchan</a></li>"
+echo "</ul>"
+echo "<p align=justify>$srclocation <a href="http://fisheye.ts.wikimedia.org/browse/mashiah">toolserver fisheye</a>.</p>"
+echo "</td><td width=75%>"
 
-parse_query category
+echo "<h1>$thish1</h1>"
+echo "$howoften<br><br>"
+echo $example
+echo "<FORM action=\"/~mashiah/cgi-bin/category.sh\" method=\"get\">"
+echo "<INPUT type=hidden name=\"interface\" value=\"$interface\">"
+echo "<P><font color=red>$catnamereq: <INPUT name=category type=\"text\"> $catnamedo</font></P>"
+echo "</FORM>"
 
 categoryurl=${category//\"/\%22}
 categorysql=${category//\"/\"\'\"\'\"}
 
 if [ "$category" != '' ]
 then
-  echo "<h4>$category</h4>"
-  echo "this is a list of all isolated articles for the category,<br />"
-  echo "see also:"
-  echo "<ul>"
-  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=disambig\">list of isolated articles <b>linked from linked disambiguation pages</b></a></li>"
-  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=interlink\">list of isolated articles <b>with linking suggestions</b></a></li>"
-  echo "<li><a href=\"/~mashiah/cgi-bin/suggest.sh?category=$categoryurl&suggest=translate\">list of isolated articles <b>with translation suggestions</b></a></li>"
-  echo "</ul>"
+  echo "<br />$submenudesc <a href=\"http://ru.wikipedia.org/w/index.php?title=Category:$categoryurl\">$category</a>"
   convertedcat=$( echo $categorysql | sed -e 's/ /_/g' )
 
   echo "<ol>"
@@ -115,16 +172,16 @@ then
                     done
                   }
   echo "</ol>"
-  echo "end of the list"
+  echo $listend
 else
-  echo "<h4>top 100 categories on isolated pages count</h4>"
+  echo "<h4>$top1name</h4>"
 
-  echo "<ul>"
+  echo "<ol>"
   {
     echo SELECT cv_title,                 \
                 cv_isocount,              \
                 100\*cv_isocount/cv_count \
-                FROM catvolume            \
+                FROM catvolume0           \
                 ORDER BY cv_isocount DESC \
                 LIMIT 100\;
   } | $sql 2>&1 | { 
@@ -132,17 +189,17 @@ else
                       do handle_catlist "$line"
                     done
                   }
-  echo "</ul>"
-  echo "end of the list"
+  echo "</ol>"
+  echo $listend
 
-  echo "<h4>top 100 categories on isolated pages ratio</h4>"
+  echo "<h4>$top2name</h4>"
 
-  echo "<ul>"
+  echo "<ol>"
   {
     echo SELECT cv_title,                            \
                 cv_isocount,                         \
                 100\*cv_isocount/cv_count as pcnt    \
-                FROM catvolume                       \
+                FROM catvolume0                      \
                 ORDER BY pcnt DESC, cv_isocount DESC \
                 LIMIT 100\;
   } | $sql 2>&1 | { 
@@ -150,15 +207,15 @@ else
                       do handle_catlist "$line"
                     done
                   }
-  echo "</ul>"
-  echo "end of the list"
+  echo "</ol>"
+  echo $listend
 fi
 
 cat << EOM
+</td>
+</tr>
+</table>
 
-<h2><a href="./category14.sh">categorytree connectivity (ruwiki)</a></h2>
-<h2><a href="./suggest.sh">suggest a link for isolated articles (ruwiki)</a></h2>
-<h2><a href="./creators.sh">isolated articles creators (ruwiki)</a></h2>
  <body>
 </html>
 EOM
