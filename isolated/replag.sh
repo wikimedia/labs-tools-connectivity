dfaabfe40023a48b5afbd198957393782df46ad5
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-05-11 23:41:44 +0000

    an utility for s3 replag.
    git-svn-id: https://svn.toolserver.org/svnroot/golem@118 2398d10b-674a-4e94-94af-0536ca92ec5b

---
+++ isolated/replag.sh
@@ -0,0 +1,22 @@
+ # 
+ # Launcher for 
+ # '''[[:ru:User:Mashiah_Davidson/toolserver/replag.sql|replag.sql]]'''.
+ # 
+ # Works on the Toolserver and outputs the replag for s3.
+ #
+ # <pre>
+
+#!/bin/bash
+
+dbhost="sql-s3"
+myusr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
+sql="mysql --host=$dbhost -A --database=u_${myusr} -n -b -N --connect_timeout=10"
+
+cat replag.sql | $sql 2>&1 | ./handle.sh
+
+#
+# Time is going and changes are required.
+#
+echo "CALL replag();" | $sql 2>&1 | ./handle.sh
+
+# </pre>
