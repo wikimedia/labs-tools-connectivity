6fb1272a0bba4a715b67dab353f1a28a0621832e
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-06-08 20:46:55 +0000

    Moved from isolated.sql Speedup Comments
    git-svn-id: https://svn.toolserver.org/svnroot/golem@158 2398d10b-674a-4e94-94af-0536ca92ec5b

---
+++ isolated/categorizer.sql
@@ -0,0 +1,134 @@
+ --
+ -- Authors: [[:ru:user:Mashiah Davidson]], still alone
+ --
+ -- <pre>
+
+ --
+ -- Significant speedup
+ --
+
+SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+
+############################################################
+delimiter //
+
+#
+# Do all the connectivity analysis.
+#
+DROP PROCEDURE IF EXISTS categories//
+CREATE PROCEDURE categories ()
+  BEGIN
+    SELECT ':: echo CATEGORIZER SETUP';
+
+    SET @starttime=now();
+
+    CALL cache_namespace_pages( 14 );
+    # fortunately for this namespace we do not depend on temporary data
+    DROP TABLE p14;
+
+    DROP TABLE IF EXISTS categories;
+    CREATE TABLE categories (
+      id int(8) unsigned NOT NULL default '0',
+      title varchar(255) binary NOT NULL default '',
+      PRIMARY KEY (id),
+      UNIQUE KEY title (title)
+    ) ENGINE=MEMORY AS
+    SELECT id,
+           title
+           FROM nr14;
+
+    # allows unique identifiers for non-existent categories
+    SELECT max(id)+1 INTO @freecatid
+           FROM categories;
+
+    SELECT CONCAT( ':: echo categories prefetch time: ', timediff(now(), @starttime));
+  END;
+//
+
+
+DROP FUNCTION IF EXISTS nrcatuid//
+CREATE FUNCTION nrcatuid (name VARCHAR(255))
+  RETURNS INT
+  DETERMINISTIC
+  BEGIN
+    DECLARE res INT;
+
+    SELECT id INTO res
+           FROM categories
+           WHERE title=name;
+
+    RETURN res;
+  END;
+//
+
+DROP PROCEDURE IF EXISTS categorylinks//
+CREATE PROCEDURE categorylinks (namespace INT)
+  BEGIN
+    DECLARE st VARCHAR(255);
+
+    SET @st=CONCAT( 'CREATE TABLE nrcatl ( nrcl_from int(8) unsigned NOT NULL default ', "'0',", ' nrcl_cat int(8) unsigned NOT NULL default ', "'0',", ' KEY (nrcl_from), KEY (nrcl_cat) ) ENGINE=MEMORY AS SELECT nr', namespace, '.id as nrcl_from, categories.id as nrcl_cat FROM ruwiki_p.categorylinks, nr', namespace, ', categories WHERE nr', namespace,'.id=cl_from and cl_to=categories.title;' );
+    PREPARE stmt FROM @st;
+    DROP TABLE IF EXISTS nrcatl;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    SELECT CONCAT( ':: echo ', count(*), ' categorizing links for non-redirect pages' )
+           FROM nrcatl;
+  END;
+//
+
+DROP PROCEDURE IF EXISTS categorystats//
+CREATE PROCEDURE categorystats (inname VARCHAR(255), outname VARCHAR(255))
+  BEGIN
+    DECLARE st VARCHAR(255);
+    SET @st=CONCAT( 'DROP TABLE IF EXISTS ', outname, ';' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    SET @st=CONCAT( 'CREATE TABLE ', outname, " ( cat int(8) unsigned NOT NULL default '0', cnt int(8) unsigned NOT NULL default '0', PRIMARY KEY (cat) ) ENGINE=MEMORY AS SELECT nrcl_cat as cat, count(DISTINCT id) as cnt FROM nrcatl0, ", inname, ' WHERE id=nrcl_from GROUP BY nrcl_cat;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    # as well as it is now applied to isolates,
+    # let remove isolated categories from the stat.
+    SET @st=CONCAT( 'DELETE ', outname, ' FROM ', outname,', ll_orcat WHERE uid=', outname, '.cat;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+  END;
+//
+
+#
+# Preparing data to use in external "isolated by category" tool.
+#
+DROP PROCEDURE IF EXISTS isolated_by_category//
+CREATE PROCEDURE isolated_by_category ()
+  BEGIN
+    SET @starttime=now();
+
+    #
+    # Amount of articles for each category.
+    # Note: Think about uncategorized articles.
+    #
+    CALL categorystats( 'articles', 'catvolume' );
+
+    CALL categorystats( 'ruwiki0', 'isocatvolume' );
+
+    DROP TABLE IF EXISTS catvolume0;
+    RENAME TABLE catvolume TO catvolume0;
+    DROP TABLE IF EXISTS isocatvolume0;
+    RENAME TABLE isocatvolume TO isocatvolume0;
+
+    CALL actuality( 'isolatedbycategory' );
+
+    SELECT CONCAT( ':: echo isolated for category web tool time: ', timediff(now(), @starttime));
+  END;
+//
+
+delimiter ;
+############################################################
+
+-- </pre>
