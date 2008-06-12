de764f93fc78d417a537fe626042f2a2d415bde7
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-06-12 22:41:29 +0000

    manual
    git-svn-id: https://svn.toolserver.org/svnroot/golem@191 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/import.sh
+++ isolated/import.sh
@@ -1,8 +1,28 @@
-#!/bin/bash
+ #
+ # Authors: [[:ru:user:Mashiah Davidson]], still alone
+ #
+ # Purpose: Data transmitter between different sql servers.
+ #
+ #          Gets input data stream from stdin, splits it into portions of
+ #          reasonable size (8192 records for now) and passes to stdout as
+ #          INSERT request to a table on receiving side with a name given
+ #          by first command line argument.
+ #
+ #          Reports on completion to communtication_exchange table on receiving
+ #          side and to stdout.
+ # 
+ # Use: Not for direct use, called from handle.sh as a result of a command like
+ #      coming from a sender.
+ #
+ #      SELECT ':: <sender-server> give <data selection statement>';
+ #
+ #      Requires receiver to be prepared with use of a command like
+ #
+ #      SELECT ':: <receiver-server> take <table-name>';
+ #
+ #      Allowed values for <sender-server> and <receiver-server> are: s2 and s3.
 
- #
- #    Authors: [[:ru:user:Mashiah Davidson]], still alone
- #
+#!/bin/bash
 
 iter=0
 portion=0;
