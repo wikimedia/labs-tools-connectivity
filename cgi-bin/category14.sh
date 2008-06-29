95c1ab51213a84dbc11f36eb52f0ed33597a8820
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2008-06-29 00:35:50 +0000

    generalized menu
    git-svn-id: https://svn.toolserver.org/svnroot/golem@210 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/category14.sh
+++ cgi-bin/category14.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+script="category14"
 source ./common
 
 parse_query networkpath
@@ -71,69 +72,12 @@ how_actual categoryspruce
 
 echo "<h1>$mainh1</h1>"
 echo "<table><tr><td width=25% border=10>"
-echo -ne "<h1>"
-if [ "$interface" = 'ru' ]
-then
-  echo -ne "<a href=\"./category14.sh?interface=en&networkpath=$networkpath\">[[en:]]</a> [[ru:]]"
-else
-  echo -ne "[[en:]] <a href=\"./category14.sh?interface=ru&networkpath=$networkpath\">[[ru:]]</a>"
-fi
-echo "</h1>"
-echo -ne "<b><a href=\"../index"
-if [ "$interface" = 'ru' ]
-then
-  echo -ne "ru"
-fi
-echo ".html\">1) $motivation</a></b><br />"
-echo "<br />"
-echo "<b>2) <a href=\"http://ru.wikipedia.org/w/index.php?title=$isourl\">$isolatedarticles</a></b><br />"
-echo "<ul>"
-echo "<li><b><a href=\"./category.sh?interface=$interface\">$bycategory</a></b></li>"
-echo "<ul>"
-echo "<li><a href=\"./suggest.sh?interface=$interface\">$allsuggestions</a></li>"
-echo "<ul>"
-echo "<li><a href=\"./suggest.sh?interface=$interface&listby=disambigcat\">$resolvedisambigs</a></li>"
-echo "<li>$justlink</li>"
-echo "<ul>"
-echo "<li><a href=\"./suggest.sh?interface=$interface&listby=interlinkcat\">$parttranslate</a></li>"
-echo "<li><a href=\"./suggest.sh?interface=$interface&listby=translatecat\">$translatenlink</a></li>"
-echo "</ul>"
-echo "</ul>"
-echo "</ul>"
-echo -ne "<li><b><a href=\"../lists"
-if [ "$interface" = 'ru' ]
-then
-  echo -ne "ru"
-fi
-echo ".html\">$wholelist</a></b></li>"
-echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/bytypes\">$byclastertype</a></b></li>"
-echo "<ul><li><a href=\"http://ru.wikipedia.org/w/index.php?title=$orphurl\">$orphanes</a></li></ul>"
-echo "<li><b><a href=\"./creators.sh?interface=$interface\">$bycreator</a></b></li>"
-echo "<li><b><a href=\"http://ru.wikipedia.org/w/index.php?title=$prjurl/cltgdata\">$graphdata</a></b></li>"
-echo "</ul>"
-echo "<br />"
-echo "<b>3) <a href=\"http://ru.wikipedia.org/w/index.php?title=$deadendurl\">$deadend</a></b><br />"
-echo "<br />"
-echo "<b>4) <a href=\"./disambig.sh?interface=$interface\">$disambig</a></b><br />"
-echo "<br />"
-if [ "$networkpath" = '' ]
-then
-  echo "<b><font color=red>5) $cattreecon</font></b><br />"
-else
-  echo "<b>5) <a href=\"./category14.sh?interface=$interface\">$cattreecon</a></b><br />"
-  echo "<ul>"
-  echo "<li><font color=red><small>$networkpath</small></font></li>"
-  echo "</ul>"
-fi
-echo "<br />"
-echo "<b>6) $contactme</b><br />"
-echo "<ul>"
-echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:Mashiah_Davidson\">$mywikipage</a></li>"
-echo "<li><a href=\"http://ru.wikipedia.org/wiki/User:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$botwikipage</a></li>"
-echo "<li><a href=\"http://ru.wikipedia.org/wiki/User Talk:%D0%93%D0%BE%D0%BB%D0%B5%D0%BC\">$commondisc</a></li>"
-echo "<li>mashiah $attext <a href="irc://irc.freenode.net/$ircchan">#$ircchan</a></li>"
-echo "</ul>"
-echo "<p align=justify>$srclocation <a href="http://fisheye.ts.wikimedia.org/browse/mashiah">toolserver fisheye</a>.</p>"
+
+#
+# The menu
+#
+the_menu
+
 echo "</td><td width=75%>"
 echo "<h1>$thish1</h1>"
 
@@ -245,6 +189,6 @@ cat << EOM
 </tr>
 </table>
 
- <body>
+ </body>
 </html>
 EOM
