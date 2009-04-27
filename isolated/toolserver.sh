4a1a20f209a760052b2c0b0c0ad0c665dc394b93
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-04-27 20:56:32 +0000

    bash api to toolserver.sql
    git-svn-id: https://svn.toolserver.org/svnroot/golem@272 2398d10b-674a-4e94-94af-0536ca92ec5b

---
+++ isolated/toolserver.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+ #
+ # Authors: [[:ru:user:Mashiah Davidson]], still alone
+ #
+ # Launcher for 
+ # '''[[:ru:User:Mashiah_Davidson/toolserver/toolserver.sql|toolserver.sql]]'''.
+ # 
+ # Works on the Toolserver and optputs server names by language.
+ #
+ # <pre>
+
+#
+# Later on this will be equal to $1, not for now.
+#
+# This variable here is just for the most stable of the servers.
+#
+
+language="ru"
+
+#
+# Indeed, when the language of interest is up, the most stable server is 
+# that one, who runs it up. 
+#
+# Later could be changed to any other constant or chosen
+# among the existent server list.
+#
+server=3
+
+#
+# Initialize variables: $dbserver, $dbhost, $usr.
+#
+# Creates sql( $server ) function.
+#
+source ../cgi-bin/ts $server
+
+rm -f debug.log no_stat.log no_templates.log no_mr.log
+
+{
+  #
+  # New language database might have to be created.
+  #
+  echo "create database if not exists u_${usr}_golem_${language};"
+
+} | $( sql $server ) 2>&1
+
+cat toolserver.sql | $( sql $server u_${usr}_golem_${language} ) 2>&1
+
+#
+# Which server language $1 database is located at?
+#
+echo "SELECT server_num( '$1' );" | $( sql $server u_${usr}_golem_${language} ) 2>&1
+
+# </pre>
