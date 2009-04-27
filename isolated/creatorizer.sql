fea92b2deb87bd4a1da985a0a8f3aad8e1f31c0b
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-04-27 21:23:44 +0000

    i18n
    git-svn-id: https://svn.toolserver.org/svnroot/golem@286 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/creatorizer.sql
+++ isolated/creatorizer.sql
@@ -23,23 +23,18 @@ delimiter //
 DROP PROCEDURE IF EXISTS creatorizer//
 CREATE PROCEDURE creatorizer ()
   BEGIN
+    DECLARE st VARCHAR(511);
+
     SELECT ':: echo CREATORIZER';
 
-    # for a set given by ruwiki0.id's look for initial revisions
+    #
+    # For a set given by ruwiki0.id's look for initial revisions
+    #
     DROP TABLE IF EXISTS firstrev;
-    CREATE TABLE firstrev (
-      title varchar(255) binary NOT NULL default '',
-      garbage int(8) unsigned NOT NULL default '0',
-      revision int(8) unsigned NOT NULL default '0',
-      PRIMARY KEY (revision)
-    ) ENGINE=MEMORY AS
-    SELECT title,
-           rev_page as garbage,
-           min(rev_id) as revision
-           FROM ruwiki_p.revision,
-                ruwiki0
-           WHERE rev_page=id
-           GROUP BY rev_page;
+    SET @st=CONCAT( 'CREATE TABLE firstrev ( title varchar(255) binary NOT NULL default ', "''", ', garbage int(8) unsigned NOT NULL default ', "'0'", ', revision int(8) unsigned NOT NULL default ', "'0'", ', PRIMARY KEY (revision) ) ENGINE=MEMORY AS SELECT title, rev_page as garbage, min(rev_id) as revision FROM ', @target_lang, 'wiki_p.revision, ruwiki0 WHERE rev_page=id GROUP BY rev_page;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 
     SELECT CONCAT( ':: echo ', count(*), ' initial revisions for isolates found' )
            FROM firstrev;
@@ -48,17 +43,10 @@ CREATE PROCEDURE creatorizer ()
     # Note: it can be done within the previous query, unfortunately
     #       containing some extra data (not yet used anywhere)
     DROP TABLE IF EXISTS creators;
-    CREATE TABLE creators (
-      title varchar(255) binary NOT NULL default '',
-      user int(8) unsigned NOT NULL default '0',
-      user_text varchar(255) binary NOT NULL default ''
-    ) ENGINE=MyISAM AS
-    SELECT title,
-           rev_user as user,
-           rev_user_text as user_text
-           FROM ruwiki_p.revision,
-                firstrev
-           WHERE revision=rev_id;
+    SET @st=CONCAT( 'CREATE TABLE creators ( title varchar(255) binary NOT NULL default ', "''", ', user int(8) unsigned NOT NULL default ', "'0'", ', user_text varchar(255) binary NOT NULL default ', "''", ' ) ENGINE=MyISAM AS SELECT title, rev_user as user, rev_user_text as user_text FROM ', @target_lang, 'wiki_p.revision, firstrev WHERE revision=rev_id;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 
     DROP TABLE firstrev;
 
