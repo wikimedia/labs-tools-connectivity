7ddb462abeeb6dfed3e7e948b01d2bbcb9b241e1
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-04-27 21:20:30 +0000

    i18n, including several sql requests formed as function and moved from cgi pages
    git-svn-id: https://svn.toolserver.org/svnroot/golem@282 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/cgi.sql
+++ isolated/cgi.sql
@@ -20,17 +20,83 @@ SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 delimiter //
 
 #
+# This procedure is being called from isolated articles categorization tool in
+# order to list all isolated articles for a given category.
+#
+DROP PROCEDURE IF EXISTS isolated_for_category//
+CREATE PROCEDURE isolated_for_category (cat_given VARCHAR(255), target_lang VARCHAR(10))
+  BEGIN
+    DECLARE st VARCHAR(255);
+
+    SET @st=CONCAT( 'SELECT title FROM ', target_lang, 'wiki_p.categorylinks, ruwiki0 WHERE id=cl_from and cl_to=', "'", cat_given, "'", ' ORDER BY title ASC;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+  END;
+//
+
+
+#
+# This procedure is being called from isolated articles categorization tool in
+# order to list all isolated articles for a given category for which there
+# are linking suggestions exist through links disambiguation.
+#
+DROP PROCEDURE IF EXISTS isolated_for_category_dsuggestable//
+CREATE PROCEDURE isolated_for_category_dsuggestable (cat_given VARCHAR(255), target_lang VARCHAR(10))
+  BEGIN
+    DECLARE st VARCHAR(255);
+
+    SET @st=CONCAT( 'SELECT DISTINCT title FROM ', target_lang, 'wiki_p.categorylinks, ruwiki0, isdis WHERE ruwiki0.id=cl_from and cl_to=', "'", cat_given, "'", ' and isdis.id=ruwiki0.id ORDER BY title ASC;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+  END;
+//
+
+
+#
+# This procedure is being called from isolated articles categorization tool in
+# order to list all isolated articles for a given category for which there
+# are linking suggestions exist as interwiki links show.
+#
+DROP PROCEDURE IF EXISTS isolated_for_category_ilsuggestable//
+CREATE PROCEDURE isolated_for_category_ilsuggestable (cat_given VARCHAR(255), target_lang VARCHAR(10))
+  BEGIN
+    DECLARE st VARCHAR(255);
+
+    SET @st=CONCAT( 'SELECT DISTINCT title FROM ', target_lang, 'wiki_p.categorylinks, ruwiki0, isres WHERE ruwiki0.id=cl_from and cl_to=', "'", cat_given, "'", ' and isres.id=ruwiki0.id ORDER BY title ASC;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+  END;
+//
+
+#
+# This procedure is being called from isolated articles categorization tool in
+# order to list all isolated articles for a given category for which there
+# are translate and linking suggestions exist as interwiki links show.
+#
+DROP PROCEDURE IF EXISTS isolated_for_category_itsuggestable//
+CREATE PROCEDURE isolated_for_category_itsuggestable (cat_given VARCHAR(255), target_lang VARCHAR(10))
+  BEGIN
+    DECLARE st VARCHAR(255);
+
+    SET @st=CONCAT( 'SELECT DISTINCT title FROM ', target_lang, 'wiki_p.categorylinks, ruwiki0, istres WHERE ruwiki0.id=cl_from and cl_to=', "'", cat_given, "'", ' and istres.id=ruwiki0.id ORDER BY title ASC;' );
+    PREPARE stmt FROM @st;
+    EXECUTE stmt;
+  END;
+//
+
+#
 # This procedure is called directly from link suggestion tool to show
 # disambiguation pages lining isolates and articles linking that disambiguation
 # pages.
 #
 DROP PROCEDURE IF EXISTS dsuggest//
-CREATE PROCEDURE dsuggest (iid VARCHAR(255))
+CREATE PROCEDURE dsuggest (iid VARCHAR(255), target_lang VARCHAR(10))
   BEGIN
+    DECLARE st VARCHAR(511);
     DECLARE done INT DEFAULT 0;
     DECLARE via_title VARCHAR(255);
     DECLARE via_id INT;
-    DECLARE cur CURSOR FOR SELECT DISTINCT page_title, a2i_via FROM isdis, ruwiki_p.page, ruwiki0 WHERE isdis.id=ruwiki0.id AND ruwiki0.title=iid AND page_id=a2i_via ORDER BY page_title ASC;
+    DECLARE cur CURSOR FOR SELECT DISTINCT name, a2i_via FROM isdis, d0site, ruwiki0 WHERE isdis.id=ruwiki0.id AND ruwiki0.title=iid AND d0site.id=a2i_via ORDER BY name ASC;
     DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
 
     OPEN cur;
@@ -41,15 +107,10 @@ CREATE PROCEDURE dsuggest (iid VARCHAR(255))
         THEN
           SELECT CONCAT( '::', via_title );
           
-          SELECT DISTINCT CONCAT( ':::' , page_title )
-                 FROM isdis,
-                      ruwiki_p.page,
-                      ruwiki0
-                 WHERE ruwiki0.title=iid and
-                       isdis.id=ruwiki0.id and
-                       a2i_via=via_id and
-                       a2i_from=page_id
-                 ORDER BY page_title ASC;
+          SET @st=CONCAT( 'SELECT DISTINCT CONCAT( ', "':::'", ' , page_title ) FROM isdis, ', target_lang, 'wiki_p.page, ruwiki0 WHERE ruwiki0.title=', "'", iid, "'", ' and isdis.id=ruwiki0.id and a2i_via=', via_id, ' and a2i_from=page_id ORDER BY page_title ASC;' );
+          PREPARE stmt FROM @st;
+          EXECUTE stmt;
+          DEALLOCATE PREPARE stmt;
 
       END IF;
     UNTIL done END REPEAT;
