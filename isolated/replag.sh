3b57945354a342c9aa30f2a499730830ff61414b
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-04-27 21:27:33 +0000

    usage changed: language code is now required -- outputs replag for a language given stores inter-run data on appropriate sql server unified mysql connection handling support
    git-svn-id: https://svn.toolserver.org/svnroot/golem@288 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/replag.sh
+++ isolated/replag.sh
@@ -5,19 +5,52 @@
  # Launcher for 
  # '''[[:ru:User:Mashiah_Davidson/toolserver/replag.sql|replag.sql]]'''.
  # 
- # Works on the Toolserver and outputs the replag for s3.
+ # Works on the Toolserver and outputs the replag for language given.
  #
  # <pre>
 
-dbhost="sql-s3"
-myusr=$( cat ~/.my.cnf | grep 'user ' | sed 's/^user = \([a-z]*\)$/\1/' )
-sql="mysql --host=$dbhost -A --database=u_${myusr} -n -b -N --connect_timeout=10"
-
-cat replag.sql | $sql 2>&1 | ./handle.sh
+#
+# This variable here is only for values logging. 
+# Currently all replag measurements are being stored in ru and this behaviour
+# could be changed when more language databases occur or on any other
+# interest.
+#
+# The language for replag calculation is completely different
+# and being reffered below in code as $1.
+#
+language="ru"
 
 #
-# Time is going and changes are required.
+# Server for connection depends on the target language
 #
-echo "CALL replag();" | $sql 2>&1 | ./handle.sh
+server=$( ./toolserver.sh "$1" )
+
+#
+# Initialize variables: $dbserver, $dbhost, $usr.
+#
+# Creates sql( $server ) function.
+#
+source ../cgi-bin/ts $server
+
+rm -f debug.log no_stat.log no_templates.log no_mr.log
+
+{
+  #
+  # New language database might have to be created.
+  #
+  echo "create database if not exists u_${usr}_golem_${language};"
+
+} | $( sql $server ) 2>&1 | ./handle.sh
+
+{
+  cat toolserver.sql
+  cat replag.sql
+} | $( sql $server u_${usr}_golem_${language} ) 2>&1 | ./handle.sh
+
+#
+# Time is the measure of change.
+# What time is it? Now you know that, and this is a change for youself.
+#
+echo "CALL replag( '$1' );" | $( sql $server u_${usr}_golem_${language} ) 2>&1 | ./handle.sh
 
 # </pre>
