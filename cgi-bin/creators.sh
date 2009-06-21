205556aa5717d20d68f0c10bb0188d20b32d4889
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-06-21 03:44:36 +0000

    Languages switching support
    git-svn-id: https://svn.toolserver.org/svnroot/golem@334 2398d10b-674a-4e94-94af-0536ca92ec5b

--- cgi-bin/creators.sh
+++ cgi-bin/creators.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-language="ru"
 script="creators"
 source ./common
 
+parse_query language
 parse_query user
 parse_query interface
 parse_query registered
 parse_query shift
-if [ "$interface" != 'ru' ]
+if [ "$interface" != 'ru' ] && [ "$interface" != 'uk' ]
 then
   interface='en'
 fi
+if [ "$language" = '' ]
+then
+  language='ru'
+fi
 
 source ./common.$interface
-source ./creators.$interface
+source ./$script.$interface
 source ./common2
 
 handle_isolates ()
@@ -24,7 +28,7 @@ handle_isolates ()
   if no_sql_error "$line"
   then
     line=${line//_/ }
-    echo "<li><a href=\"http://ru.wikipedia.org/w/index.php?title=$line\" target=\"_blank\">$line</a> <small><a href=\"./suggest.sh?interface=$interface&title=$line\"><font color=green>[[$suggest]]</font></a></small></li>"
+    echo "<li><a href=\"http://$language.wikipedia.org/w/index.php?title=$line\" target=\"_blank\">$line</a> <small><a href=\"./suggest.sh?interface=$interface&title=$line\"><font color=green>[[$suggest]]</font></a></small></li>"
   fi
 }
 
@@ -37,7 +41,7 @@ handle_userlist ()
     local utxt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\1/g' )
     local uid=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\2/g' )
     local amnt=$( echo $line | sed -e 's/^\(.*\+\) \([0-9]\+\) \([0-9]\+\)/\3/g' )
-    echo "<li><a href='./creators.sh?interface=$interface&user=$utxt&registered=$uid'>$utxt</a>: $amnt</li>"
+    echo "<li><a href='./creators.sh?language=$language&interface=$interface&user=$utxt&registered=$uid'>$utxt</a>: $amnt</li>"
   fi
 }
 
@@ -64,7 +68,16 @@ cat << EOM
 EOM
 how_actual creatorizer
 
-echo "<h1>$mainh1</h1>"
+#
+# Switching between interface languages at the top right
+#
+if_lang
+
+#
+# The page header at the center
+#
+the_page_header
+
 echo "<table><tr><td width=25% border=10>"
 
 #
@@ -80,6 +93,7 @@ echo "$whatisit<br><br>"
 echo $example
 echo "<FORM action=\"./creators.sh\" method=\"get\">"
 echo "<INPUT type=hidden name=\"interface\" value=\"$interface\">"
+echo "<INPUT type=hidden name=\"language\" value=\"$language\">"
 echo "<P><font color=red>$unamereq: <INPUT name=user type=\"text\"> $unamedo</font></P>"
 echo "</FORM>"
 
@@ -96,9 +110,9 @@ then
   echo -ne "<br />$list1expl "
   if [ "$registered" = '0' ]
   then
-    echo "<a href='http://ru.wikipedia.org/wiki/Special:Contributions/$user' target=\"_blank\">$user</a>"
+    echo "<a href='http://$language.wikipedia.org/wiki/Special:Contributions/$user' target=\"_blank\">$user</a>"
   else
-    echo "<a href=\"http://ru.wikipedia.org/w/index.php?title=User:$userurl\" target=\"_blank\">$user</a>"
+    echo "<a href=\"http://$language.wikipedia.org/w/index.php?title=User:$userurl\" target=\"_blank\">$user</a>"
   fi
   echo "<ol>"
   {
@@ -118,9 +132,9 @@ else
 
   if [ $((shift)) -gt 0 ]
   then
-    echo "<a href=\"./creators.sh?shift=$shiftprev&interface=$interface\">$previous 100</a> "
+    echo "<a href=\"./creators.sh?language=$language&interface=$interface&shift=$shiftprev\">$previous 100</a> "
   fi
-  echo "<a href=\"./creators.sh?shift=$shiftnext&interface=$interface\">$next 100</a>"
+  echo "<a href=\"./creators.sh?language=$language&interface=$interface&shift=$shiftnext\">$next 100</a>"
   echo "<ol start=$((shift+1))>"
   {
     echo SELECT user_text,         \
@@ -138,9 +152,9 @@ else
   echo "</ol>"
   if [ $((shift)) -gt 0 ]
   then
-    echo "<a href=\"./creators.sh?shift=$shiftprev&interface=$interface\">$previous 100</a> "
+    echo "<a href=\"./creators.sh?language=$language&interface=$interface&shift=$shiftprev\">$previous 100</a> "
   fi
-  echo "<a href=\"./creators.sh?shift=$shiftnext&interface=$interface\">$next 100</a>"
+  echo "<a href=\"./creators.sh?language=$language&interface=$interface&shift=$shiftnext\">$next 100</a>"
 fi
 
 cat << EOM
