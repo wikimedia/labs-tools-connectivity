944ae7bdd5b59d487abfc902e1387edcf73399f8
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2009-07-08 22:15:33 +0000

    fix to the previous commit: CORE:  - simplification of internal table contents
    git-svn-id: https://svn.toolserver.org/svnroot/golem@406 2398d10b-674a-4e94-94af-0536ca92ec5b

--- isolated/creatorizer.sql
+++ isolated/creatorizer.sql
@@ -25,13 +25,15 @@ CREATE PROCEDURE creatorizer ()
   BEGIN
     DECLARE st VARCHAR(511);
 
+    SET @starttime=now();
+
     SELECT ':: echo CREATORIZER';
 
     #
     # For a set given by ruwiki0.id's look for initial revisions
     #
     DROP TABLE IF EXISTS firstrev;
-    SET @st=CONCAT( 'CREATE TABLE firstrev ( title varchar(255) binary NOT NULL default ', "''", ', garbage int(8) unsigned NOT NULL default ', "'0'", ', revision int(8) unsigned NOT NULL default ', "'0'", ', PRIMARY KEY (revision) ) ENGINE=MEMORY AS SELECT title, rev_page as garbage, min(rev_id) as revision FROM ', @dbname, '.revision, ruwiki0 WHERE rev_page=id GROUP BY rev_page;' );
+    SET @st=CONCAT( 'CREATE TABLE firstrev ( iid int(8) unsigned NOT NULL default ', "'0'", ', garbage int(8) unsigned NOT NULL default ', "'0'", ', revision int(8) unsigned NOT NULL default ', "'0'", ', PRIMARY KEY (revision) ) ENGINE=MEMORY AS SELECT id as iid, rev_page as garbage, min(rev_id) as revision FROM ', @dbname, '.revision, ruwiki0 WHERE rev_page=id GROUP BY rev_page;' );
     PREPARE stmt FROM @st;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
@@ -43,7 +45,7 @@ CREATE PROCEDURE creatorizer ()
     # Note: it can be done within the previous query, unfortunately
     #       containing some extra data (not yet used anywhere)
     DROP TABLE IF EXISTS creators;
-    SET @st=CONCAT( 'CREATE TABLE creators ( title varchar(255) binary NOT NULL default ', "''", ', user int(8) unsigned NOT NULL default ', "'0'", ', user_text varchar(255) binary NOT NULL default ', "''", ' ) ENGINE=MyISAM AS SELECT title, rev_user as user, rev_user_text as user_text FROM ', @dbname, '.revision, firstrev WHERE revision=rev_id;' );
+    SET @st=CONCAT( 'CREATE TABLE creators ( iid int(8) unsigned NOT NULL default ', "'0'", ', user int(8) unsigned NOT NULL default ', "'0'", ', user_text varchar(255) binary NOT NULL default ', "''", ' ) ENGINE=MyISAM AS SELECT iid, rev_user as user, rev_user_text as user_text FROM ', @dbname, '.revision, firstrev WHERE revision=rev_id;' );
     PREPARE stmt FROM @st;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
@@ -57,6 +59,8 @@ CREATE PROCEDURE creatorizer ()
 
     SELECT CONCAT( ':: echo ', count(DISTINCT user, user_text), ' isolated articles creators found' )
            FROM creators0;
+
+    SELECT CONCAT( ':: echo creatorizer web tool time: ', timediff(now(), @starttime));
   END;
 //
 
