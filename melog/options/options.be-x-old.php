0a3ca9d2c57744b12405395cfae6ddab3c2b356f
mashiah <mashiah@2398d10b-674a-4e94-94af-0536ca92ec5b>
2012-10-06 20:23:57 +0000

    MELOG: - even more templates like 'Fuck off from my article now' for uk
    git-svn-id: https://svn.toolserver.org/svnroot/golem@509 2398d10b-674a-4e94-94af-0536ca92ec5b

--- melog/options/options.be-x-old.php
+++ melog/options/options.be-x-old.php
@@ -7,7 +7,7 @@ class Options_be_x_old extends Options {
 	public function skipGlobal($text) {
 		/*$this->_text = $text;
 		
-		if(Template::testTemplates(strtolower($this->_text), array('delete','выдаліць'))) { // TODO: get delete nominations through i18n
+		if(Template::testTemplates(strtolower($this->_text), array('delete','выдаліць'))) { // TODO: get nominations through i18n
 			pecho('Article is nominated for deletion.', PECHO_LOG);
 			return true;
 		}*/
